fabe9fe8a3976e6cabc2b05a2db26df5018a9518
mechtatell <62153475+mechtatell@users.noreply.github.com>
2021-03-16 15:19:24 +0400

    add helpful files

--- PI22_Kalachikov_Ivan_lab_04/routes/employee.routes.js
+++ PI22_Kalachikov_Ivan_lab_04/routes/employee.routes.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 router.post("/employee", passport.authenticate('jwt', {session: false}), employeeController.createEmployee)
 router.get("/employees", employeeController.getEmployees)
 router.get("/employees/:dep_id", employeeController.getEmployeesOnDepartment)
-router.get("/employee/:id", passport.authenticate('jwt', {session: false}), employeeController.getEmployee)
+router.get("/employee/:id", employeeController.getEmployee)
 router.put("/employee", passport.authenticate('jwt', {session: false}), employeeController.updateEmployee)
 router.delete("/employee/:id", passport.authenticate('jwt', {session: false}), employeeController.deleteEmployee)
 
