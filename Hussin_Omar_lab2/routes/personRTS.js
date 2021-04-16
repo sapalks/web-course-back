ad443973c78e0426c6613ddec6726e1b627e20b8
Hussen-omar-bibd-22-csharp <70457726+A-O8@users.noreply.github.com>
2021-04-16 17:02:41 +0400

    Update personRTS.js

--- Hussin_Omar_lab2/routes/personRTS.js
+++ Hussin_Omar_lab2/routes/personRTS.js
@@ -2,8 +2,8 @@ const Router = require('express')
 const router = new Router();
 const controller = require('../controller/personCTRL')
 
-router.get('/persons', controller.geAllpersons) //получает список все студентов
-router.get('/persons/:roomsID', controller.getpersonInroomes) //получает всех студентов по указанной специальности
+router.get('/persons', controller.geAllpersons) 
+router.get('/persons/:roomsID', controller.getpersonInroomes) 
 router.get('/person/:id', controller.getperson) //получает студента по id
 router.post('/person', controller.createperson) //создает запись с новым студентом
 router.put('/person', controller.updateperson) //редактирует запись в таблице студентов
