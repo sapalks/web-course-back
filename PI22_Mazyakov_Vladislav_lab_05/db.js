0422e750d6a43deeaf511e9474f60e72b162faad
Quenby-apr <70429313+Quenby-apr@users.noreply.github.com>
2021-05-02 16:35:25 +0400

    Delete PI22_Mazyakov_Vladislav_lab_05 directory

--- PI22_Mazyakov_Vladislav_lab_05/db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool
-
-const pool = new Pool ({
-    host: "localhost",
-    user: "postgres",
-    password: "1",
-    database: "forip"
-})
-module.exports = pool;
--- PI22_Mazyakov_Vladislav_lab_05/routes/user.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const userController = require('../controller/user')
-
-router.post('/register', userController.register)
-router.post('/login', userController.login)
-
-module.exports = router
