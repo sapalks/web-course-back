a4c83fe8d137086985b1a2cff1bb27100e33372a
BeyondDesolation <lightnight19@yandex.ru>
2021-05-15 11:03:54 +0400

    ой не в ту ветку

--- PI22_Lyamkin_Andrey_lab_04/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const users = require('../controllers/user.controller.js')
-
-router.post('/register', users.register)
-router.post('/login', users.login)
-
-module.exports = router
