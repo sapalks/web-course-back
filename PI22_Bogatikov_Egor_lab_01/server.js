9bea8f6a8d2e732584870f8768471c5f223ef84a
Егор Богатиков <egorbogatikov@mail.ru>
2021-05-16 18:07:12 +0400

    lab_01_ready

---
+++ PI22_Bogatikov_Egor_lab_01/server.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var winston = require('winston');
+const { format } = require('winston');
+const { printf } = format;
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var logger = winston.createLogger({
+    format: printf(({message}) => {
+        return ` ${message}`;
+    }),
+    transports: [
+        new winston.transports.File({
+            filename: 'logs.log',
+            handleExceptions: true
+        })
+    ]
+})
+
+app.listen(4578, function () {
+    logger.info("{" + new Date().toUTCString() + "} Server started on port 4578")
+})
+
+var logging = function (req, res, next) {
+    logger.info("{" + new Date().toUTCString() + "} {" + req.ip + "} {" + req.method + "} {" + req.protocol + '://' + req.get('host') + req.originalUrl + "} {" + res.statusCode + "}");
+    next();
+};
+
+app.use(logging);
+
+
+var calculate = function (value1, value2, operation) {
+    var result;
+    switch (operation) {
+        case 'addition':
+            result = value1 + value2
+            break;
+        case 'subtraction':
+            result = value1 - value2
+            break;
+        case 'multiplication':
+            result = value1 * value2
+            break;
+        case 'division':
+            result = value1 / value2
+            break;
+        default:
+            return ({status:'error', body:'Wrong ' + operation})
+    }
+
+    if (!Number.isFinite(result)) {
+        return({status:'error', body:'incorrect values'})
+    }
+
+    return {status:'ok',body:result};
+}
+
+app.listen(() => {
+    console.log(`Example app listening at http://localhost:4578`)
+})
+
+var dayOfTheWeek = function (day) {
+
+    var days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+
+    var date = new Date();
+    var answer = new Date(date.getFullYear(), date.getMonth(), day);
+    return days[answer.getDay()]
+}
+
+app.get('/ping', function (req, res) {
+    res.send({
+        status: 'ok'
+    })
+})
+
+app.get('/weekday', function (req, res) {
+    if (Number(req.query.day) < 1 || Number(req.query.day) > 31) {
+        throw new Error("incorrect values")
+    }
+    res.send(dayOfTheWeek(Number(req.query.day)));
+})
+
+app.post('/calc', function (req, res) {
+    res.send(calculate(req.body.value1, req.body.value2, req.body.operation));
+})
+
+app.use(function(err, req, res, next) {
+    logger.error("{" + new Date().toUTCString() + "} {" + req.ip + "} {" + req.method + "} {" + req.protocol + '://' + req.get('host') + req.originalUrl + "}\n" + err.stack);
+    res.status(400).send(err.message);
+});
