92f6e98d3cff12bc2491b7eef825d8159bab8ddb
UlStudent <ksergeiii@mail.ru>
2021-05-22 18:05:58 +0300

    lab 6 is ready

---
+++ PI22_Kyanzhin_Sergei_lab_06/routes/oauth.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router()
+const passport = require("passport")
+const oauth = require('../controller/oauth.js')
+
+router.get('/index', oauth.index)
+router.get('/login', passport.authenticate('yandex'))
+router.get('/logout', oauth.logout)
+router.get('/auth', oauth.auth)
+router.get('/tokens', oauth.tokens)
+router.get('/updateToken', oauth.updateToken)
+
+module.exports = router
