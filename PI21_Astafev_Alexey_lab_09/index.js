808babc52f0a780f14fdff7a0c64b7ff63a17a42
Alexey Astafev <31274867+astalexey@users.noreply.github.com>
2021-05-25 19:43:14 +0400

    PI21_Astafev_Alexey_lab_09
    PI21_Astafev_Alexey_lab_09

---
+++ PI21_Astafev_Alexey_lab_09/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const dateFormat = require('dateformat')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const userModel = require('./model/user')
+
+const chatBot = 'Chat'
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket) {
+
+    socket.on('sending-message', function(message) {
+        const user = userModel.getUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, message))
+    });
+
+    socket.on('join', function({ username, room }) {
+
+        // check the uniqueness of the name
+        if (!isUniqName(username, room)) {
+            socket.emit('error')
+            return
+        }
+
+        //const user = createUser(socket.id, username, room)
+        const user = userModel.createUser(socket.id, username, room)
+
+        // subscribe the socket to a given channel
+        socket.join(user.room)
+        socket.emit('first-connection')
+
+        // sending events to all the connected clients other than the sender
+        socket.broadcast.to(user.room).emit('message', formatMessage(chatBot, `${user.username} connected to the chat`))
+
+        updatingListUsers(user.room)
+    });
+
+    socket.on('disconnect', function() {
+        const user = userModel.deleteUser(socket.id)
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(chatBot, `${user.username} disconnected from chat`))
+            updatingListUsers(user.room)
+        }
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is working on http://localhost:${port}`)
+});
+
+Date.prototype.format = function (mask, utc) {
+    return dateFormat(this, mask, utc)
+}
+
+function formatMessage(username, text) {
+    return { username, text, time: new Date().format('HH:MM') }
+}
+
+function updatingListUsers(room) {
+    io.to(room).emit('list-users', { users: userModel.getRoomUsers(room) })
+}
+
+function isUniqName(username, room) {
+    if(userModel.getUsers().find(user => user.username === username && user.room === room)) {
+        return false
+    }
+    return true
+}
---
+++ PI21_Astafev_Alexey_lab_09/public/socket.js
@@ -0,0 +1,95 @@
+const form = document.getElementById('message-sending-form')
+const socket = io()
+const roomName = document.getElementById('room-name')
+const numberOfUsers = document.getElementById('number-of-users')
+const usr = document.getElementById('user-name')
+const user = document.getElementById('user')
+const messages = document.querySelector('.messages')
+const popup = document.getElementById('mypopup')
+const popupOnline = document.getElementById('users-online')
+const popupClose = document.querySelector('.close')
+const input = document.getElementById('msg');
+const params = (new URL(document.location)).searchParams
+const username = params.get('username')
+const room = params.get('room')
+
+
+usr.innerHTML = username
+roomName.innerText = room
+
+socket.emit('join', { username, room })
+
+socket.on('list-users', function({users}) {
+    user.innerHTML = `${users.map(user => `<p class="user"><i class="fas fa-user"></i> ${user.username}</p>`).join('')}`
+    numberOfUsers.innerHTML = users.length
+})
+
+socket.on('message', function(msg) {
+    defineUser(msg)
+    messages.scrollTop = messages.scrollHeight
+})
+
+socket.on('first-connection', function() {
+    Swal.fire({
+        icon: 'success',
+        title: `U are connected to chat ${room}!`
+    })
+})
+
+socket.on('error', function() {
+    let timerInterval
+    Swal.fire({
+        icon: 'error',
+        title: 'User with the same name already exists!',
+        html: 'U will be automatically disconnect from the server after <b></b> milliseconds.',
+        timer: 5000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            timerInterval = setInterval(() => {
+                const content = Swal.getContent()
+                if (content) {
+                    const b = content.querySelector('b')
+                    if (b) {
+                        b.textContent = Swal.getTimerLeft()
+                    }
+                }
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            window.location = "index.html"
+        }
+    })
+    document.getElementById('send').disabled = true;
+    document.querySelector('input').disabled = true;
+})
+
+popupOnline.onclick = function() {
+    popup.style.display = 'block'
+}
+
+popupClose.onclick = function() {
+    popup.style.display = 'none'
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    socket.emit('sending-message', e.target.elements.msg.value)
+    input.value = ''
+    input.focus()
+})
+
+function defineUser(msg) {
+    const div = document.createElement('div')
+    if(msg.username === username) {
+        div.classList.add('my-message')
+    } else {
+        div.classList.add('message')
+    }
+    div.innerHTML = `<p class="meta"> ${msg.username} <span class='time'>${msg.time}</span></p><p class="text">${msg.text}</p>`
+    document.querySelector('.messages').appendChild(div)
+}
