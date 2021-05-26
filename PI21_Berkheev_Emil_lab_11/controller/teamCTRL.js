91a7f342c4cdf7eac22a45c88b956d01757e595b
InnerWisdom <68237197+InnerWisdom@users.noreply.github.com>
2021-05-26 17:31:36 +0400

    Update teamCTRL.js

--- PI21_Berkheev_Emil_lab_11/controller/teamCTRL.js
+++ PI21_Berkheev_Emil_lab_11/controller/teamCTRL.js
@@ -20,14 +20,14 @@ class teamController {
         }
         const id = req.params.id;
         const result = await db.query('SELECT * FROM team WHERE isDeleted = FALSE AND teamID = $1', [id]);
-        cache.save('team', result.rows[0])
+        cache.save('team ${id}', result.rows[0])
         res.json(result.rows[0]);
     }
 
     async createTeam(req, res) {
         const {name} = req.body;
         const result = await db.query('INSERT INTO team(name) VALUES ($1) RETURNING *', [name]);
-        cache.delete('team')
+        cache.delete('team ${id}')
         cache.delete('teams')
         res.json(result.rows[0]);
     }
@@ -35,7 +35,7 @@ class teamController {
     async updateTeam(req, res) {
         const {teamID, name} = req.body;
         const result = await db.query('UPDATE team SET name = $1 WHERE teamID = $2 AND isDeleted = FALSE RETURNING *', [name, teamID]);
-        cache.delete('team')
+        cache.delete('team ${id}')
         cache.delete('teams')
         res.json(result.rows[0]);
     }
@@ -43,7 +43,7 @@ class teamController {
     async deleteTeam(req, res) {
         const id = req.params.id;
         await db.query('UPDATE team SET isDeleted = TRUE WHERE teamID = $1 RETURNING *', [id]);
-        cache.delete('team')
+        cache.delete('team ${id}')
         cache.delete('teams')
         res.json('team with id: ' + id + ' was deleted');
     }
