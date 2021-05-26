8fbacb57882024c3dba029033376cc29b0971ad0
InnerWisdom <68237197+InnerWisdom@users.noreply.github.com>
2021-05-26 18:02:00 +0400

    Update teamCTRL.js

--- PI21_Berkheev_Emil_lab_11/controller/teamCTRL.js
+++ PI21_Berkheev_Emil_lab_11/controller/teamCTRL.js
@@ -4,47 +4,46 @@ const cache = require('../cache.js')
 class teamController {
 
     async geAllTeams(req, res) {
-        const cached = await cache.get('teams')
+        const cached = await cache.get(`teams`)
         if (cached) {
             return res.json(cached)
         }
         const result = await db.query('SELECT * FROM team WHERE isDeleted = FALSE');
-        cache.save('teams', result.rows)
+        cache.save(`teams`, result.rows)
         res.json(result.rows);
     }
 
     async getTeam(req, res) {
-        const cached = await cache.get('team')
+        const cached = await cache.get(`team ${id}`)
         if (cached) {
             return res.json(cached)
         }
         const id = req.params.id;
         const result = await db.query('SELECT * FROM team WHERE isDeleted = FALSE AND teamID = $1', [id]);
-        cache.save('team ${id}', result.rows[0])
+        cache.save(`team ${id}`, result.rows[0])
         res.json(result.rows[0]);
     }
 
     async createTeam(req, res) {
         const {name} = req.body;
         const result = await db.query('INSERT INTO team(name) VALUES ($1) RETURNING *', [name]);
-        cache.delete('team ${id}')
-        cache.delete('teams')
+        cache.delete(`teams`)
         res.json(result.rows[0]);
     }
 
     async updateTeam(req, res) {
         const {teamID, name} = req.body;
         const result = await db.query('UPDATE team SET name = $1 WHERE teamID = $2 AND isDeleted = FALSE RETURNING *', [name, teamID]);
-        cache.delete('team ${id}')
-        cache.delete('teams')
+        cache.delete(`team ${id}`)
+        cache.delete(`teams`)
         res.json(result.rows[0]);
     }
 
     async deleteTeam(req, res) {
         const id = req.params.id;
         await db.query('UPDATE team SET isDeleted = TRUE WHERE teamID = $1 RETURNING *', [id]);
-        cache.delete('team ${id}')
-        cache.delete('teams')
+        cache.delete(`team ${id}`)
+        cache.delete(`teams`)
         res.json('team with id: ' + id + ' was deleted');
     }
     async isCached(req, res) {
