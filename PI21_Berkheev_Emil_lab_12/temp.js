5496bd990b1f15c9dd54e4d6397e8cf61f2216ba
InnerWisdom <68237197+InnerWisdom@users.noreply.github.com>
2021-05-26 09:29:11 +0400

    Delete temp.js
    забыл удалить неиспользуемый скрипт

--- PI21_Berkheev_Emil_lab_12/temp.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-var base64_convert = require('base64-js'); 
-var content = fs.readFileSync("test/Image_00001.jpg");
-console.log(base64_convert.fromByteArray(content));
-
- 
