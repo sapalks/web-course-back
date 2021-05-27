2f99009a7366cb8bf9bb6f089af619000c2cec6a
Artem-pibd <70539271+Artem-pibd@users.noreply.github.com>
2021-05-27 11:45:36 +0400

    fix

--- PI22_Dyrnochkin_Artem_lab_02/controller/bookCTRL.js
+++ PI22_Dyrnochkin_Artem_lab_02/controller/bookCTRL.js
@@ -3,38 +3,38 @@ const db = require('../db')
 class bookController {
 
     async geAllBooks(req, res) {
-        const result = await db.query('SELECT * FROM book WHERE isDeleted = FALSE');
+        const result = await db.query('SELECT bookID,name_book,genre,page_count,authorID FROM book WHERE isDeleted = FALSE');
         res.json(result.rows);
     }
 
     async getBookInAuthors(req, res) {
         const id = req.params.AuthorID;
-        const result = await db.query('SELECT * FROM book WHERE isDeleted = FALSE AND authorID = $1', [id]);
+        const result = await db.query('SELECT bookID,name_book,genre,page_count,authorID FROM book WHERE isDeleted = FALSE AND authorID = $1', [id]);
         res.json(result.rows);
     }
 
     async getBook(req, res) {
         const id = req.params.id;
-        const result = await db.query('SELECT * FROM book WHERE isDeleted = FALSE AND bookID = $1', [id]);
+        const result = await db.query('SELECT bookID,name_book,genre,page_count,authorID FROM book WHERE isDeleted = FALSE AND bookID = $1', [id]);
         res.json(result.rows[0]);
     }
 
     async createBook(req, res) {
         const { name_book, genre, page_count, authorID } = req.body;
-        const result = await db.query('INSERT INTO book(name_book, genre, page_count, authorID) VALUES ($1, $2, $3, $4) RETURNING *', [name_book, genre, page_count, authorID]);
+        const result = await db.query('INSERT INTO book(name_book, genre, page_count, authorID) VALUES ($1, $2, $3, $4) RETURNING bookID,name_book,genre,page_count,authorID', [name_book, genre, page_count, authorID]);
         res.json(result.rows[0]);  
     }
 
     async updateBook(req, res) {
         const {bookID, name_book, genre, page_count, authorID} = req.body;
-        const result = await db.query('UPDATE book SET name_book = $1, genre = $2, page_count = $3 , authorID = $4, WHERE bookID = $5 AND isDeleted = FALSE RETURNING *',
+        const result = await db.query('UPDATE book SET name_book = $1, genre = $2, page_count = $3 , authorID = $4, WHERE bookID = $5 AND isDeleted = FALSE RETURNING bookID,name_book,genre,page_count,authorID',
         [name_book, genre, page_count, authorID, bookID]);
         res.json(result.rows[0]);
     }
 
     async deleteBook(req, res) {
         const id = req.params.id;
-        await db.query('UPDATE book SET isDeleted = TRUE WHERE bookID = $1 RETURNING *', [id]);
+        await db.query('UPDATE book SET isDeleted = TRUE WHERE bookID = $1 RETURNING bookID,name_book,genre,page_count,authorID', [id]);
         res.json('book with id: ' + id + ' was deleted');
     }
 }
