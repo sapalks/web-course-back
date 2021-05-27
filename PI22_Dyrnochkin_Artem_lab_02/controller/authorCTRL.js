b910d37e4b6ad0a2cd100c164d9a5dcb91bef974
Artem-pibd <70539271+Artem-pibd@users.noreply.github.com>
2021-05-27 11:45:09 +0400

    fix

--- PI22_Dyrnochkin_Artem_lab_02/controller/authorCTRL.js
+++ PI22_Dyrnochkin_Artem_lab_02/controller/authorCTRL.js
@@ -3,31 +3,31 @@ const db = require('../db')
 class authorController {
 
     async geAllAuthors(req, res) {
-        const result = await db.query('SELECT * FROM author WHERE isDeleted = FALSE');
+        const result = await db.query('SELECT authorID,name FROM author WHERE isDeleted = FALSE');
         res.json(result.rows);
     }
 
     async getAuthor(req, res) {
         const id = req.params.id;
-        const result = await db.query('SELECT * FROM author WHERE isDeleted = FALSE AND authorID = $1', [id]);
+        const result = await db.query('SELECT authorID,name FROM author WHERE isDeleted = FALSE AND authorID = $1', [id]);
         res.json(result.rows[0]);
     }
 
     async createAuthor(req, res) {
         const {name} = req.body;
-        const result = await db.query('INSERT INTO author(name) VALUES ($1) RETURNING *', [name]);
+        const result = await db.query('INSERT INTO author(name) VALUES ($1) RETURNING authorID,name', [name]);
         res.json(result.rows[0]);
     }
 
     async updateAuthor(req, res) {
         const {authorID, name} = req.body;
-        const result = await db.query('UPDATE author SET name = $1 WHERE authorID = $2 AND isDeleted = FALSE RETURNING *', [name,authorID]);
+        const result = await db.query('UPDATE author SET name = $1 WHERE authorID = $2 AND isDeleted = FALSE RETURNING authorID,name', [name,authorID]);
         res.json(result.rows[0]);
     }
 
     async deleteAuthor(req, res) {
         const id = req.params.id;
-        await db.query('UPDATE author SET isDeleted = TRUE WHERE authorID = $1 RETURNING *', [id]);
+        await db.query('UPDATE author SET isDeleted = TRUE WHERE authorID = $1 RETURNING authorID,name', [id]);
         res.json('author with id: ' + id + ' was deleted');
     }
 }
