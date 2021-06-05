8cfa86cab322718fc7fb753bcf49d8e2816b5902
A-O8 <omerhanesh40@gmail.com>
2021-06-05 12:59:24 +0400

    PIbd22_Hussin_Omar_lab3

--- PIbd22_Hussin_Omar_lab3/routes/roomRTS.js
+++ PIbd22_Hussin_Omar_lab3/routes/roomRTS.js
@@ -2,10 +2,10 @@ const Router = require('express')
 const router = new Router();
 const controller = require('../controller/roomCTRL')
 
-router.get('/rooms', controller.getAllrooms) //получает список всех специальностей
-router.get('/room/:id', controller.getroom) //получает специальность по id
-router.post('/room', controller.createroom) //создает запись с новой специальностью
-router.put('/room', controller.updateroom) //редактирует запись в таблице специальностей
-router.delete('/room/:id', controller.deleteroom) //помечает запись со специальностью удаленной
+router.get('/rooms', controller.getAllrooms) 
+router.get('/room/:id', controller.getroom) 
+router.post('/room', controller.createroom) 
+router.put('/room', controller.updateroom) 
+router.delete('/room/:id', controller.deleteroom) 
 
 module.exports = router;
