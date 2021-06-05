6dc9aaa4b01fb22d52ae92e37545697297031edb
2oublePeace <vova.emiryan@yandex.ru>
2021-06-05 14:10:15 +0400

    initial commit

---
+++ PI21_Emiryan_Vladimir_lab_02/database.sql
@@ -0,0 +1,13 @@
+create TABLE task(
+    id SERIAL PRIMARY KEY,
+    theme VARCHAR(255),
+    timeOfRemind TIMESTAMP,
+    deadline TIMESTAMP
+);
+
+create TABLE step(
+    id SERIAL PRIMARY KEY,
+    theme VARCHAR(255)
+    task_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES task (id)
+);
