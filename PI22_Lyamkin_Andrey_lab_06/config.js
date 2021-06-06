734781d532c186473ba13ebbf4ac1b648a6ae24b
BeyondDesolation <lightnight19@yandex.ru>
2021-06-06 17:57:19 +0400

    PI22 Lyamkin lab6 is ready

---
+++ PI22_Lyamkin_Andrey_lab_06/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    CLIENT_ID : "9bb5ec2ede6e479caef86dc9ab73c3bd",
+    CLIENT_SECRET : "c8085a8727124100960df9f684cd6e06",
+    CALLBACK_URL : "http://localhost:3000/callback"
+};
