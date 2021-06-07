009b854f17a3f7b801ddc3bd40053946acbfe26e
2oublePeace <vova.emiryan@yandex.ru>
2021-06-07 14:51:53 +0400

    done

--- PI21_Emiryan_Vladimir_lab_02/index.js
+++ PI21_Emiryan_Vladimir_lab_02/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const taskRouter = require('./routes/task.routes')
+const stepRouter = require('./routes/step.routes')
 
 const PORT = process.env.PORT || 3000
 
@@ -7,5 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use('/api', taskRouter)
+app.use('/api', stepRouter)
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`))
