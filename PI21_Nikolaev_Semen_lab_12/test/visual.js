56d988be6779f8eba8af050389092665341317e4
Jambo-TimDevelopment <jambo-timDevelopment@yandex.ru>
2021-06-12 11:41:31 +0400

    PIbd-21 Nikolaev Semen lab 12

---
+++ PI21_Nikolaev_Semen_lab_12/test/visual.js
@@ -0,0 +1,22 @@
+const recognize = require('../index')
+const fs = require('fs')
+const base64_js = require('base64-js')
+var config = {}
+
+describe('Тест', function () {
+    this.timeout(10000);
+    it('Распознование текста на изображении', function (done) {
+        var base64_picture = base64_js.fromByteArray(fs.readFileSync("test/3.png"));
+        config.Bearer = 't1.9euelZqbkY6ajo6NxozMkcbPi8iLmO3rnpWanZqMmMaKjseXmpaYjo7Ni47l8_dmH3J5-e9xUl16_N3z9yZOb3n573FSXXr8.g9dwL8lp77tljZT9HaZuQZGAnACDtcwXgltREvrjptHGox6SCgtf9_wxTAanKwFSTQCZMgE-TM4eE-JFtagKBg'; 
+        config.folderId = 'b1gmk9bm5a5t5s437og6';
+        recognize(base64_picture, config, function (err, text) {
+            try {
+                console.log(text)
+            } catch (e) {
+                done(e);
+                return;
+            }
+            done();
+        });
+    });
+});
