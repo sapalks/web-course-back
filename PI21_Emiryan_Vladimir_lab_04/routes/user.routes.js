6f501ec61dcd905c82d1c3bb0abc39b8e759a436
2oublePeace <vova.emiryan@yandex.ru>
2021-06-13 21:43:40 +0400

    init

--- PI21_Emiryan_Vladimir_lab_04/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router();
-const controller = require('../controller/user.controller.js')
-
-router.post('/register', controller.register)
-router.post('/login', controller.login)
-
-module.exports = router;
